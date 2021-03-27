a3da1635c0c88e89cd904a3c1c54150a6d922814
BrunoSantoni <brunosantoni98@gmail.com>
2021-03-27 15:09:57 -0300

    feat(fauna): FaunaDB adicionado para salvar os usuários

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,19 +1,20 @@
-import styles from './styles.module.scss';
-
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
+import { signIn, signOut, useSession } from 'next-auth/client';
+
+import styles from './styles.module.scss';
 
 export function SignInButton() {
-  const isUserLoggedIn = true;
+  const [session] = useSession(); // Retorna se o usuário tem uma sessão.
 
-  return isUserLoggedIn ? (
-    <button className={styles.signInButton}>
+  return session ? (
+    <button className={styles.signInButton} onClick={() => signOut()}>
       <FaGithub color="#04d361" />
-      <span>Bruno Santoni</span>
+      <span>{session.user.name}</span>
       <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
-    <button className={styles.signInButton}>
+    <button className={styles.signInButton} onClick={() => signIn('github')}>
       <FaGithub color="#eba417" />
       <span>Sign in with Github</span>
     </button>
